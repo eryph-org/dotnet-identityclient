8e142018b5eccbd0f473509a52091751c2f43b22
Christopher Mann <christopher.mann@dbosoft.eu>
2024-05-28 18:09:26 +0200

    Update generation script

--- gen/generate_local.ps1
+++ gen/generate_local.ps1
@@ -1,8 +1,30 @@
-Push-Location $PSScriptRoot
-$settings = Get-Content -Raw -Path "config.json" | ConvertFrom-Json
-cd ..
-$location = Get-Location
+#Requires -Version 7.4
+<#
+    .SYNOPSIS
+        Generates the REST client locally
+    .DESCRIPTION
+        This script generates the REST client locally.
+#>
+[CmdletBinding()]
+param()
+
+$PSNativeCommandUseErrorActionPreference = $true
+$ErrorActionPreference = 'Stop'
+
+$settings = Get-Content -Raw -Path "$PSScriptRoot/config.json" | ConvertFrom-Json
 $tag = $settings.tag
 $spec = $settings.spec
 
-autorest  ..\eryph-api-spec\specification\$spec --tag=$tag --csharp-src-folder=$location --v3 --csharp --verbose
+npm exec --package="autorest" -- `
+    autorest `
+    --version="3.10.2" `
+    --use="@autorest/csharp@3.0.0-beta.20240207.2" `
+    --use="@autorest/modelerfour@4.27.0" `
+    "$PSScriptRoot/../../eryph-api-spec/specification/$spec" `
+    --tag=$tag `
+    --csharp-src-folder="$PSScriptRoot/.." `
+    --v3 `
+    --csharp `
+    --skip-csproj=true `
+    --generate-sample-project=false `
+    --verbose
